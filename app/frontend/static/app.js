1022d66bff7ef8053c2b869bff843d0422382a07
mamdouhhz <mamdouhhazemm@gmail.com>
2026-07-04 21:53:11 +0300

    terraform, k8s, ansinle done, remianing jenkins & monitoring

--- app/frontend/static/app.js
+++
@@ -1,9 +0,0 @@
-fetch("/api/visits")
-    .then(res => res.json())
-    .then(data => {
-        document.getElementById("count").textContent = data.visit_count;
-    })
-    .catch(err => {
-        document.getElementById("count").textContent = "error";
-        console.error(err);
-    });
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -21,3 +21,8 @@ output "ecr_repository_arn" {
 output "jenkins_ec2_public_ip" {
   value = module.jenkins_ec2.public_ip
 }
+
+output "rds_endpoint" {
+  description = "The endpoint of the RDS instance"
+  value       = module.rds.rds_endpoint
+}
